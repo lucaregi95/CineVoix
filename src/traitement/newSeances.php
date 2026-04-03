884705161fda699b4423ea0f1e2e81beb12b73d8
FAJNZYN_Pr <P.FAJNZYN@lprs.fr>
2026-04-03 16:13:05 +0200

    ajout des fichiers de gestion des seances

--- src/traitement/newSeances.php
+++ src/traitement/newSeances.php
@@ -1,1 +1,7 @@
 <?php
+require_once '../bdd/Bdd.php';
+require_once '../modele/SeancesRepository.php';
+
+if(isset($_POST['date_seance'])){
+    $seance = new Seances(null, $_POST['date_seance']);
+}
