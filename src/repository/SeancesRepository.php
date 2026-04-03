d8894357c5b0d37b9138eed570865ead2506663a
lucaregi95 <lucaregi95@gmail.com>
2026-04-03 16:09:09 +0200

    feature: mise en place des seances

---
+++ src/repository/SeancesRepository.php
@@ -0,0 +1,41 @@
+<?php
+class SeancesRepository
+{
+    private $connexionBdd;
+    public function __construct(){
+        $this->connexionBdd = (new Bdd())->getConnexionBdd();
+    }
+
+    public function getSeances($seances){
+        $sql = "SELECT * FROM seances WHERE id_seance = :seances";
+        $req = $this->connexionBdd->prepare($sql);
+        $req->bindValue(':seances', $seances);
+        $req->execute();
+        $result = $req->fetch();
+        $seance = new Seances($result["id_seance"], $result["date_naissance"], $result["ref_film"], $result["ref_salle"]);
+        return $seance;
+    }
+
+    public function getAllSeances(){
+        $sql = "SELECT * FROM seances";
+        $req = $this->connexionBdd->prepare($sql);
+        $req->execute();
+        $results = $req->fetchAll();
+        $tabSeances = array();
+        foreach($results as $result){
+            $seances = new Seances($result["id_seance"], $result["date_naissance"], $result["ref_film"], $result["ref_salle"]);
+            $tabSeances[] = $seances;
+        }
+        return $tabSeances;
+    }
+
+    public function ajouterReservation(Seances $seance){
+        $sql = "INSERT INTO VALUES :id_seance, :date_seance, :ref_film, :ref_salle";
+        $req = $this->connexionBdd->prepare($sql);
+        $req->bindValue(':id_seance', $seance->getIdSeance());
+        $req->bindValue(':date_seance', $seance->getDateSeance());
+        $req->bindValue(':ref_film', $seance->getRefFilm());
+        $req->bindValue(':ref_salle', $seance->getRefSalle());
+        $req->execute();
+    }
+}
