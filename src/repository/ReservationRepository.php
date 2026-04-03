95516891fe8bfcc7d82dd5b54ac34794136f3916
FAJNZYN_Pr <P.FAJNZYN@lprs.fr>
2026-04-03 15:18:34 +0200

    fix() : ajout des fichiers de reservation

--- src/repository/ReservationRepository.php
+++ src/repository/ReservationRepository.php
@@ -1,1 +1,42 @@
 <?php
+class ReservationRepository
+{
+    private $connexionBdd;
+    public function __construct(){
+        $this->connexionBdd = (new Bdd())->getConnexionBdd();
+    }
+
+    public function getReservation($id_reservation){
+        $sql = "SELECT * FROM reservation WHERE id_reservation = :id_reservation";
+        $req = $this->connexionBdd->prepare($sql);
+        $req->bindValue(':id_reservation', $id_reservation);
+        $req->execute();
+        $result = $req->fetch();
+        $reservation = new Reservation($result["id_reservation"], $result["statut"], $result["qte_plein_tarif"], $result["qte_etudiant"], $result["qte_senior"]);
+        return $reservation;
+    }
+
+    public function getAllReservations(){
+        $sql = "SELECT * FROM reservation";
+        $req = $this->connexionBdd->prepare($sql);
+        $req->execute();
+        $results = $req->fetchAll();
+        $tabReservations = array();
+        foreach($results as $result){
+            $reservation = new Reservation($result["id_reservation"], $result["statut"], $result["qte_plein_tarif"], $result["qte_etudiant"], $result["qte_senior"]);
+        $tabReservations[] = $reservation;
+        }
+        return $tabReservations;
+    }
+
+    public function ajouterReservation(Reservation $reservation){
+        $sql = "INSERT INTO VALUES :id_reservation, :statut, :qte_plein_tarif, :qte_etudiant, :qte_senior";
+        $req = $this->connexionBdd->prepare($sql);
+        $req->bindValue(':id_reservation', $reservation->getIdReservation());
+        $req->bindValue(':statut', $reservation->getStatut());
+        $req->bindValue(':qte_plein_tarif', $reservation->getQtePleinTarif());
+        $req->bindValue(':qte_etudiant', $reservation->getQteEtudiant());
+        $req->bindValue(':qte_senior', $reservation->getQteSenior());
+        $req->execute();
+    }
+}
--- src/traitement/newReservation.php
+++ src/traitement/newReservation.php
@@ -1,1 +1,7 @@
 <?php
+require_once '../bdd/Bdd.php';
+require_once '../modele/Reservation.php';
+
+if (isset($_POST['statut']) && isset($_POST['qte_plein_tarif']) && isset($_POST['qte_etudiant']) && isset($_POST['qte_senior'])) {
+    $reservation = new Reservation(null, $_POST['statut'], $_POST['qte_plein_tarif'], $_POST['qte_etudiant'], $_POST['qte_senior']);
+}
