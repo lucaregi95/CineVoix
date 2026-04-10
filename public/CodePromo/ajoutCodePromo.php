e8eea241c66f4d1f7986ab9304bd3d171437e8c4
FAJNZYN_Pr <P.FAJNZYN@lprs.fr>
2026-04-10 14:19:40 +0200

    changement du titre de l'onglet

--- public/CodePromo/ajoutCodePromo.php
+++ public/CodePromo/ajoutCodePromo.php
@@ -17,7 +17,7 @@ $_POST['code']=null;
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Cinémoi - Accueil</title>
+    <title>Cinémoi - Ajout Code Promo</title>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
 </head>
 <body>
---
+++ src/traitement/newActeurs.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__.'/../../bdd/Bdd.php';
+require_once __DIR__.'/../../modele/CodePromo.php';
+
+
+if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) && isset($_POST['mdp']) && isset($_POST['tel']) && isset($_POST['rue']) && isset($_POST['cp']) && isset($_POST['ville']) && isset($_POST['date_naissance']) && isset($_POST['role']) && isset($_POST['etat']) &&  isset($_POST['date_creation'])) {
+    $acteur = new Acteurs(null, $_POST['nom'], $_POST['prenom'], $_POST['email'], $_POST['mdp'], $_POST['date_naissance'], $_POST['tel'], $_POST['rue'], $_POST['ville'], $_POST['cp']);
+}
+?>
