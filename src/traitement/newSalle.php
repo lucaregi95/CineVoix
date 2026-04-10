faa1ceafdd4714afce37d42c9dfd46335928341c
FAJNZYN_Pr <P.FAJNZYN@lprs.fr>
2026-04-10 14:24:49 +0200

    changement des require_once et des if isset

--- src/traitement/newSalle.php
+++ src/traitement/newSalle.php
@@ -1,7 +1,7 @@
 <?php
-require_once '../bdd/Bdd.php';
-require_once '../modele/Salle.php';
+require_once __DIR__.'/../bdd/Bdd.php';
+require_once __DIR__.'/../modele/Salle.php';
 
-if(isset($_POST['code']) && isset($_POST['nom']) && isset($_POST['capacite']) && isset($_POST['etat'])){
-    $salle = new Salle ($_POST['code'], $_POST['nom'], $_POST['capacite'], $_POST['etat']);
+if(isset($_POST['code']) && isset($_POST['nom']) && isset($_POST['capacite'])){
+    $salle = new Salle (null, $_POST['code'], $_POST['nom'],$_POST['capacite'], true);
 }
