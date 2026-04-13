4d51ab92a1cde762129ec4feb278b271eb00da8c
Prosper <prosperfajnzyn@gmail.com>
2026-04-13 02:22:42 +0200

    creation du fichier de traitement de la connexion

--- public/Acteurs/connexionActeur2.php
+++ public/Acteurs/connexionActeur2.php
@@ -1,1 +1,60 @@
 <?php
+require_once('../../src/bdd/Bdd.php');
+require_once('../../src/modele/Acteurs.php');
+require_once('../../src/repository/ActeursRepository.php');
+
+// Récupération des données du formulaire de connexion
+if (isset($_POST["email"])) {
+    $email = $_POST["email"];
+} else {
+    $email = '';
+}
+
+if (isset($_POST["mdp"])) {
+    $mdp = $_POST["mdp"];
+} else {
+    $mdp = '';
+}
+
+if(isset($_POST["page"])){
+    $page  = $_POST["page"];}
+
+// Requête préparée : sélectionne l'utilisateur correspondant
+$rep = new ActeursRepository();
+$result = $rep->connecterActeur($email, $mdp);
+
+if (!$result) {
+    // Aucun utilisateur trouvé : redirection avec message d'erreur
+    header("Location: connexionActeur.php?erreur=unknown");
+} else {
+
+    session_start();
+    // Stockage des informations de l'utilisateur en session
+    $_SESSION['id']     = $result["id_acteur"];
+    $_SESSION['nom']    = $result["nom"];
+    $_SESSION['prenom'] = $result["prenom"];
+    $_SESSION['email']  = $result["email"];
+    $_SESSION['tel'] = $result["tel"];
+    $_SESSION['rue']   = $result["rue"];
+    $_SESSION['cp']   = $result["cp"];
+    $_SESSION['ville']   = $result["ville"];
+    $_SESSION['date_naissance']   = $result["date_naissance"];
+    $_SESSION['role']   = $result["role"];
+    $_SESSION['date_creation']   = $result["date_creation"];
+
+    header('Location: tabActeur.php');
+
+
+
+
+    ?>
+    <!DOCTYPE html>
+    <html lang=fr>
+    <head>
+        <meta charset="UTF-8">
+        <title>Connexion en Cours</title>
+    </head>
+    <body>
+    </body>
+    </html>
+<?php } ?>
