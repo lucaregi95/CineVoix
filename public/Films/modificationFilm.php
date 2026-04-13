67404307f06b1bd97d45dc6df68c1f79d36206f5
Prosper <prosperfajnzyn@gmail.com>
2026-04-13 02:25:13 +0200

    refactor du nom de flim a film ainsi que les noms des fichiers. changement du nom d'onglet. fermeture de div(s)

--- public/Films/modificationFilm.php
+++ public/Films/modificationFilm.php
@@ -14,11 +14,11 @@ if (isset($film)) {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Cinémoi - Accueil</title>
+    <title>Cinémoi - Modifcation Film</title>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
 </head>
 <body>
-<form action="ajoutFlim.php" method="post">
+<form action="ajoutFilm.php" method="post">
 
     <div class="row">
         <div class="col-md-6">
@@ -34,8 +34,33 @@ if (isset($film)) {
                 <label>duree :</label>
                 <input type="time" name="duree" required>
             </div>
+            <div class="mb-3">
+                <label>Affiche :</label>
+                <input type="file" name="affiche">
+            </div>
+            <div class="mb-3">
+                <label>Genre :</label>
+                <input type="text" name="genre">
+            </div>
+            <div class="mb-3">
+                <label>Age minimum :</label>
+                <input type="number" name="age_min">
+            </div>
+            <div class="mb-3">
+                <label>Réalisateur :</label>
+                <input type="text" name="realisateur">
+            </div>
+            <div class="mb-3">
+                <label>Date de sortie :</label>
+                <input type="date" name="date_sortie">
+            </div>
+            <div class="mb-3">
+                <label>Bande-annonce :</label>
+                <input type="url" name="bande_annonce">
+            </div>
             <button type="submit">Valider</button>
-
+        </div>
+    </div>
 </form>
 </body>
 </html>
